b7e7a35b0e756843e3283494edd2564bf1e84bf6
Stephen Cirner <scirner@getcrossbeam.com>
2018-09-13 23:41:52 -0400

    Get real responses printing and in a table format.

---
+++ src/iex.rs
@@ -0,0 +1,57 @@
+use hyper::rt::{Future, Stream};
+use hyper::{self, client::HttpConnector};
+use hyper_tls::HttpsConnector;
+use serde_json;
+
+const BASE_URL: &str = "https://api.iextrading.com/1.0";
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct IexResponse {
+    symbol: String,
+    pub delayed_price: f32,
+    open: f32,
+    ytd_change: f32,
+}
+
+impl IexResponse {
+    pub fn get_symbol(&self) -> &str {
+        self.symbol.as_str()
+    }
+
+    pub fn ytd_percentage(&self) -> f32 {
+        self.ytd_change * 100.0
+    }
+
+    pub fn daily_percentage(&self) -> f32 {
+        ((self.delayed_price - self.open) / self.open) * 100.0
+    }
+}
+
+#[derive(Clone)]
+pub struct IexClient {
+    // TODO wrap with Rc
+    client: hyper::Client<HttpsConnector<HttpConnector>, hyper::Body>,
+}
+
+impl IexClient {
+    pub fn new() -> Self {
+        let https = HttpsConnector::new(4).unwrap();
+        let client = hyper::Client::builder().build(https);
+        IexClient { client }
+    }
+
+    pub fn fetch_stock(self, ticker: &str) -> impl Future<Item = IexResponse, Error = ()> {
+        let uri = format!("{}/stock/{}/quote", BASE_URL, ticker);
+        let uri = uri.parse::<hyper::Uri>().unwrap();
+        self.client
+            .get(uri)
+            .and_then(|res| res.into_body().concat2())
+            .and_then(|body| {
+                let s = ::std::str::from_utf8(&body).expect("httpbin sends utf-8 JSON");
+                let v: IexResponse = serde_json::from_str(s).unwrap();
+                Ok(v)
+            })
+            .map_err(|err| println!("{:?}", err))
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,30 @@
 extern crate clap;
+extern crate futures;
+extern crate hyper;
+extern crate hyper_tls;
+#[macro_use]
+extern crate prettytable;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
 
 use clap::{App, Arg};
+use hyper::rt::{self, Future};
+use prettytable::Table;
+
+mod iex;
 
 fn main() {
+    let mut default_config = env::home_dir().unwrap();
+    default_config.push(".stockcli");
+    default_config.push("config");
+    default_config.set_extension("csv");
+
     let matches = App::new("stockcli")
         .version("0.1.0")
         .author("Stephen Cirner <scirner22@gmail.com>")
@@ -25,13 +47,43 @@ fn main() {
         )
         .get_matches();
 
-    if let Some(symbols) = matches.value_of("symbols") {
-        let symbols: Vec<&str> = symbols.split(',').collect();
-        print!("symbols {:?}", symbols);
+    let symbols: Vec<String>;
+    if let Some(s) = matches.value_of("symbols") {
+        symbols = s.split(',').map(String::from).collect();
     } else {
         let config = matches
             .value_of("config")
-            .unwrap_or("~/.stockcli/config.csv");
-        print!("config {}", config);
+            .unwrap_or_else(|| default_config.to_str().unwrap());
+        let mut file = File::open(config).expect("config file not found");
+        let mut s = String::new();
+        file.read_to_string(&mut s).unwrap();
+        symbols = s
+            .trim()
+            .split(',')
+            .map(|s| String::from(s.trim()))
+            .collect();
     }
+
+    let client = iex::IexClient::new();
+    let futures = symbols.into_iter().map(move |sym| {
+        let client = client.clone();
+        client.fetch_stock(&sym)
+    });
+    let f = futures::future::join_all(futures)
+        .map(|results| {
+            let mut table = Table::new();
+            table.add_row(row!["SYMBOL", "PRICE", "DAILY", "YTD"]);
+            for res in results {
+                table.add_row(row![
+                    res.get_symbol(),
+                    format!("{:.2}", res.delayed_price),
+                    format!("{:.2}%", res.daily_percentage()),
+                    format!("{:.2}%", res.ytd_percentage()),
+                ]);
+            }
+            table.printstd();
+        })
+        .map_err(|err| println!("{:?}", err));
+
+    rt::run(f);
 }
